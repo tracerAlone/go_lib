fe0efa14ad2ee812d66a0b2319e788ba30cd64c8
tracer <hhushen7@gmail.com>
2022-07-02 16:09:47 +0800

    add some lib tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tracerAlone/go_lib
+
+go 1.16
---
+++ hello.go
@@ -0,0 +1,8 @@
+package go_lib
+
+import "fmt" 
+
+// say hello world
+func sayHelloWorld() string {
+   return fmt.Sprintf("Hi, %s", "shenweiyang, hello world!")
+}
